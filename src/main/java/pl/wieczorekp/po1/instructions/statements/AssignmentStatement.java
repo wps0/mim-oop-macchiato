db1a45c2c9b87f2f04f65587a80e2d192699b53f
wps0 <53907986+wps0@users.noreply.github.com>
2023-04-28 23:01:10 +0200

    add more statements

---
+++ src/main/java/pl/wieczorekp/po1/instructions/statements/AssignmentStatement.java
@@ -0,0 +1,37 @@
+package pl.wieczorekp.po1.instructions.statements;
+
+import pl.wieczorekp.po1.instructions.ExecutionEndedException;
+import pl.wieczorekp.po1.instructions.expressions.Expression;
+
+public class AssignmentStatement extends Statement {
+    private String varName;
+    private Expression value;
+    private boolean hasEnded;
+
+    public AssignmentStatement(CodeBlock context, String varName, Expression value) {
+        super(context);
+        this.varName = varName;
+        this.value = value;
+        this.hasEnded = false;
+    }
+
+    @Override
+    public void executeOne() {
+        if (hasEnded()) {
+            throw new ExecutionEndedException();
+        }
+
+        context.assignVariable(varName, value.evaluateInContext(context));
+        hasEnded = true;
+    }
+
+    @Override
+    protected boolean hasEnded() {
+        return hasEnded;
+    }
+
+    @Override
+    public String toString() {
+        return "var " + varName + " := " + value + " (hasEnded=" + hasEnded + ")";
+    }
+}
---
+++ src/main/java/pl/wieczorekp/po1/instructions/statements/BlockStatement.java
@@ -0,0 +1,11 @@
+package pl.wieczorekp.po1.instructions.statements;
+
+import java.util.Optional;
+
+public abstract class BlockStatement extends Statement {
+    protected BlockStatement(CodeBlock context) {
+        super(context);
+    }
+
+    public abstract Optional<Statement> getCurrentStatement();
+}
